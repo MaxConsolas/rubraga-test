daba9e8af3d8f3a398d848490fc56e6738d62cfd
Maxim Consolas <maxim.consolas@mvplab.org>
2025-04-28 15:50:34 +0000

    feat: done

--- resources/js/components/Room.vue
+++ resources/js/components/Room.vue
@@ -2,12 +2,12 @@
   <Alert class="flex flex-col">
     <AlertTitle class="w-full">
       <div class="flex justify-between">
-        <span>{{ room.type.name }}</span>
+        <span>{{ room?.type?.name ?? '' }}</span>
         <span class="text-muted-foreground">{{ room.price }} руб / ночь</span>
       </div>
     </AlertTitle>
     <AlertDescription>
-      {{ room.type.description }}
+      {{ room?.type?.description ?? '' }}
       <div class="mt-6 flex gap-4 items-baseline justify-between w-full">
         <h2 class="text-xl font-semibold tracking-tight text-card-foreground">{{ room.price * (days ?? 1) }} руб</h2>
         <Button @click="selectRoom(room.id)" size="sm" class="cursor-pointer">
--- resources/js/components/RoomList.vue
+++ resources/js/components/RoomList.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="flex flex-col gap-4 mt-12 max-h-[400px] overflow-y-auto" v-auto-animate>
-    <template v-if="!loading">
+  <div class="flex flex-col gap-4 mt-12 max-h-[400px] overflow-y-auto" v-auto-animate="{ duration: 500 }">
+    <!-- <template v-if="!loading"> -->
       <Room
         v-for="room in list"
+        :key="room.id"
         :room="room"
         :days="days"
         @select="(room) => $emit('pickRoom', room)"
       />
-    </template>
+    <!-- </template> -->
   </div>
 </template>
 
